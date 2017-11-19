274d9a59cc327700a7374c0d33e17b253ed44edc
Abdallah Bari <abdallah.bari@gmail.com>
2017-11-19 12:27:11 -0500

    Add files via upload

---
+++ BDA_Chapter_01.R
@@ -0,0 +1,48 @@
+chapter 01
+setwd()
+getwd()
+# Reading and conveting files (HTML) to R Data Frame format
+# Content of files such as HTML are by tags  <tag>content</tag>, such tags can be identifed in the case of a table coentent by html_nodes(): 
+
+library(rvest)
+stock.trend.html <- read_html("https://finance.yahoo.com/quote/AAPL/history?p=AAPL")
+head(stock.trend.html)
+stock.trend.table <- html_nodes(stock.trend.html, "table")
+head(stock.trend.table)
+str(stock.trend.table)
+
+stock.trend.table <- stock.trend.html %>%
+  html_nodes("table") %>%
+  .[1:1] %>%
+  html_table(fill = TRUE)
+str(stock.trend.df)
+stock.trend.df <- data.frame(stock.trend.table)
+str(stock.trend.df)
+head(stock.trend.df)
+names(stock.trend.df)
+
+stock.trend.df$Date <- as.Date(stock.trend.df$Date, format = "%b %d, %Y")
+stock.trend.df$Open <- as.numeric(stock.trend.df$Open)
+stock.trend.df$High <- as.numeric(stock.trend.df$High)
+stock.trend.df$Low <- as.numeric(stock.trend.df$Low)
+stock.trend.df$Close.  <- as.numeric(stock.trend.df$Close.)    
+stock.trend.df$Adj.Close.. <- as.numeric(stock.trend.df$Adj.Close..)
+stock.trend.df$Volume  <-  as.numeric(stock.trend.df$Volume)      
+
+
+# Date conversion in R 
+# Code	Value
+# %d	Day of the month (decimal number)
+# %m	Month (decimal number)
+# %b	Month (abbreviated)
+# %B	Month (full name)
+# %y	Year (2 digit)
+# %Y	Year (4 digit)
+
+stock.trend.df <- na.omit(stock.trend.df)
+str(stock.trend.df)
+head(stock.trend.df)
+names(stock.trend.df)
+
+write.csv(stock.trend.df,'stock.trend.df.csv')
+write.table(stock.trend.df, "stock.trend.df.txt", sep="\t")
